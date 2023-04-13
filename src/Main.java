969f149c9a211130226b5bfe4ef807b413131b2c
kamil <kamilolszowiec123@gmail.com>
2023-04-13 15:42:07 +0200

    3.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        CustomList <String> list=new CustomList<>();
-        list.addLast("abc");
-        list.addLast("def");
+        CustomList <Integer> list=new CustomList<>();
+        list.addLast(32);
+        list.addLast(64);
         System.out.println(list.getLast());
         System.out.println(list.removeFirst());
         System.out.println(list.getFirst());
+        System.out.println(list.iterator().next());
+        list.stream().map(o->100+o).forEach(System.out::println);
 
     }
 }
