9dfc1c1905873a7d606d0612218ad7861ff2844f
kamil <kamilolszowiec123@gmail.com>
2023-04-13 15:00:26 +0200

    3.

--- src/CustomList.java
+++ src/CustomList.java
@@ -1,4 +1,33 @@
-public class CustomList <T> {
+import java.util.AbstractList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
+public class CustomList<T> extends AbstractList<T> {
+    @Override
+    public T get(int i){
+        Node thisNode=head;
+        for(int k=0;k<i;++k){
+            if(thisNode!=null){
+                thisNode=thisNode.next;
+            }
+            else{
+                throw new NoSuchElementException();
+            }
+        }
+        return thisNode.value;
+    }
+    @Override
+    public int size(){
+        Node thisNode=head;
+        int result=0;
+        while(thisNode!=null){
+            thisNode=thisNode.next;
+            result++;
+        }
+        return result;
+    }
+
     private class Node{
         T value;
         Node next;
@@ -61,5 +90,33 @@ public class CustomList <T> {
         tail=newLast;
         return last;
 
+    }
+    @Override
+    public boolean add(T t) {
+        addLast(t);
+        return true;
+    }
+    public Iterator<T> iterator(){
+        return new Iterator<T>(){
+            Node tempNode=head;
+            @Override
+            public boolean hasNext() {
+                return tempNode!=null;
+            }
+
+            @Override
+            public T next() {
+                T value=tempNode.value;
+                tempNode=tempNode.next;
+                return value;
+            }
+        };
+    }
+    public Stream<T> stream(){
+        Stream.Builder<T> stream=Stream.builder();
+        for(T s: this){
+            stream.accept(s);
+        }
+        return stream.build();
     }
 }
